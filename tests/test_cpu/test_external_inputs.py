615622894125b32d3ba101761eff9ac1e83f633b
Michał Pstrąg <m.pstrag.kontakt@gmail.com>
2021-11-21 17:39:25 +0100

    Change ext inputs tests

--- tests/test_cpu/test_external_inputs.py
+++ tests/test_cpu/test_external_inputs.py
@@ -4,51 +4,65 @@ from nes.bus import Bus
 
 @pytest.fixture
 def nes():
-    return Bus()
+    nes = Bus()
+    
+    # Set reset vector:
+    nes.ram[0xFFFC] = 0x00
+    nes.ram[0xFFFD] = 0x80
+    
+    # Reset:
+    nes.cpu.reset()
+    while nes.cpu.clock(): pass
+    
+    return nes
 
-def test_reset(nes):
+def test_reset():
     """
     Tests CPU registers just after power and changes during reset,
     and that RAM isn't changed during reset.
     """
-    # After power up:
-    assert nes.cpu.a_reg == 0x00
-    assert nes.cpu.x_reg == 0x00
-    assert nes.cpu.y_reg == 0x00
-    assert nes.cpu.sp_reg == 0xFD
-    assert nes.cpu.status_reg == 0x00 | CPU.FLAGS.U.value | CPU.FLAGS.I.value
-    
+    # Given:
+    cycles_used = 1
+    nes = Bus()
+    nes.ram[0xFFFC] = 0x00
+    nes.ram[0xFFFD] = 0x80
     ram_cp = nes.ram.copy()
-    nes.cpu.reset()
     
-    # After reset:
+    # When:
+    nes.cpu.reset()
+    while nes.cpu.clock():
+        cycles_used += 1
+    
+    # Then:
+    assert cycles_used == 8
     assert nes.ram == ram_cp
     assert nes.cpu.a_reg == 0x00
     assert nes.cpu.x_reg == 0x00
     assert nes.cpu.y_reg == 0x00
     assert nes.cpu.sp_reg == 0xFD
-    assert nes.cpu.pc_reg == (nes.ram[0xFFFD] << 8) | nes.ram[0xFFFC]
+    assert nes.cpu.pc_reg == 0x8000
     assert nes.cpu.status_reg == 0x00 | CPU.FLAGS.U.value | CPU.FLAGS.I.value
     
-def test_interrupts(nes):
+def test_nonmaskable_interrupts(nes):
     """
-    Tests the behavior and timing of CPU in the presence of interrupts,
-    both IRQ and NMI.
+    Tests the behavior and timing of CPU in the presence of interrupts - NMI.
     """
-    # TODO Test IRQ 
+    # Given:
+    cycles_used = 1
+    nes.ram[0xFFFA] = 0x90
+    nes.ram[0xFFFB] = 0xFF
     
-    pc = nes.cpu.pc_reg
+    # When:
     nes.cpu.nonmaskable_interrupt_request()
+    while nes.cpu.clock():
+        cycles_used += 1
     
-    # Test stack content:
-    assert nes.ram[0x0100 + nes.cpu.sp_reg + 3] == (pc >> 8) & 0x00FF
-    assert nes.ram[0x0100 + nes.cpu.sp_reg + 2] == pc & 0x00FF
-    assert nes.ram[0x0100 + nes.cpu.sp_reg + 1] == nes.cpu.status_reg
-    
-    # Test status register flags:
+    # Then:
+    assert cycles_used == 8
+    assert nes.cpu.pc_reg == 0xFF90
+    assert nes.ram[0x01FD] == 0x80
+    assert nes.ram[0x01FC] == 0x00
+    assert nes.ram[0x01FB] == nes.cpu.status_reg  
     assert not (nes.cpu.status_reg & CPU.FLAGS.B.value)
     assert nes.cpu.status_reg & CPU.FLAGS.U.value
     assert nes.cpu.status_reg & CPU.FLAGS.I.value
-    
-    # Test PC value:
-    assert nes.cpu.pc_reg == (nes.ram[0xFFFB] << 8) | nes.ram[0xFFFA]
