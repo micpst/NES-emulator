295351e272e108269d84b424783bc4402e36e018
Michał Pstrąg <m.pstrag.kontakt@gmail.com>
2021-12-13 21:58:40 +0100

    Update dependencies

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         name="NES emulator",
-        description=" NES emulator written in Python",
+        description="NES emulator written in Python",
         author="Michał Pstrąg",
         license="MIT",
         license_file="LICENSE.md",
-        packages = ["nes"],
+        packages=["nes", "nes/mappers"],
         classifiers=[
             "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: MIT License"
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent"
         ],
-        install_requires=["numpy"],
+        install_requires=["pygame"],
         python_requires=">=3.7",
         extras_require={
             "testing": ["pytest", "mypy"]
