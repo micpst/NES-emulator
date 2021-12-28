2be09dc2414e8b7805611d013909ebd5ff588896
Michał Pstrąg <m.pstrag.kontakt@gmail.com>
2021-12-28 22:22:34 +0100

    Add palette and some fixes

--- nes/ppu.py
+++ nes/ppu.py
@@ -1,5 +1,5 @@
 import pygame as pg
-from typing import Optional, List
+from typing import Optional, List, Tuple
 from .cartridge import Cartridge
 
 
@@ -9,12 +9,83 @@ class PPU:
     """
 
     def __init__(self) -> None:
+        # PPU bus:
         self.name_table: List[List[int]] = 2 * [1024 * [0x00]]
         self.pattern_table: List[List[int]] = 2 * [4096 * [0x00]]
-        self.palette_table: List[int] = 32 * [0x00]
+        self.palette_table: List[Tuple[int, int, int]] = [
+            ( 84,  84,  84),
+            (  0,  30, 116),
+            (  8,  16, 144),
+            ( 48,   0, 136),
+            ( 68,   0, 100),
+            ( 92,   0,  48),
+            ( 84,   4,   0),
+            ( 60,  24,   0),
+            ( 32,  42,   0),
+            (  8,  58,   0),
+            (  0,  64,   0),
+            (  0,  60,   0),
+            (  0,  50,  60),
+            (  0,   0,   0),
+            (  0,   0,   0),
+            (  0,   0,   0),
+
+            (152, 150, 152),
+            (  8,  76, 196),
+            ( 48,  50, 236),
+            ( 92,  30, 228),
+            (136,  20, 176),
+            (160,  20, 100),
+            (152,  34,  32),
+            (120,  60,   0),
+            ( 84,  90,   0),
+            ( 40, 114,   0),
+            (  8, 124,   0),
+            (  0, 118,  40),
+            (  0, 102, 120),
+            (  0,   0,   0),
+            (  0,   0,   0),
+            (  0,   0,   0),
+
+            (236, 238, 236),
+            ( 76, 154, 236),
+            (120, 124, 236),
+            (176,  98, 236),
+            (228,  84, 236),
+            (236,  88, 180),
+            (236, 106, 100),
+            (212, 136,  32),
+            (160, 170,   0),
+            (116, 196,   0),
+            ( 76, 208,  32),
+            ( 56, 204, 108),
+            ( 56, 180, 204),
+            ( 60,  60,  60),
+            (  0,   0,   0),
+            (  0,   0,   0),
+
+            (236, 238, 236),
+            (168, 204, 236),
+            (188, 188, 236),
+            (212, 178, 236),
+            (236, 174, 236),
+            (236, 174, 212),
+            (236, 180, 176),
+            (228, 196, 144),
+            (204, 210, 120),
+            (180, 222, 120),
+            (168, 226, 144),
+            (152, 226, 180),
+            (160, 214, 228),
+            (160, 162, 160),
+            (  0,   0,   0),
+            (  0,   0,   0),
+        ]
         self.cart: Optional[Cartridge] = None
 
+        # Helper variables:
         self._cycles: int = 0
+        self._clock_count: int = 0
         self._scanline: int = 0
         self._frame_complete: bool = False
 
@@ -22,7 +93,9 @@ class PPU:
         self.cart = cart
 
     def clock(self) -> None:
+        self._clock_count += 1
         self._cycles += 1
+
         if self._cycles >= 341:
             self._cycles = 0
             self._scanline += 1
@@ -84,7 +157,10 @@ class PPU:
         return 0x00
 
     def _write(self, address: int, data: int) -> None:
-        self.cart.write(address & 0x3FFF, data)
+        if self.cart:
+            self.cart.write(address & 0x3FFF, data)
 
     def _read(self, address: int, read_only: bool) -> int:
-        return self.cart.read(address & 0x3FFF)
+        if self.cart:
+            return self.cart.read(address & 0x3FFF)
+        return 0x00
